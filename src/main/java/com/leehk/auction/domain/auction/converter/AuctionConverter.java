e60eca9104d2b496e7eea174090dc8cfdf5cd378
이학준 <dlgkrwns213@naver.com>
2025-09-20 01:14:04 +0900

    feat: AuctionConverter 클래스에 Dto와 Domain 간 변환 메서드 추가

--- src/main/java/com/leehk/auction/domain/auction/converter/AuctionConverter.java
+++ src/main/java/com/leehk/auction/domain/auction/converter/AuctionConverter.java
@@ -1,6 +1,7 @@
 package com.leehk.auction.domain.auction.converter;
 
 import com.leehk.auction.domain.auction.domain.Auction;
+import com.leehk.auction.domain.auction.dto.AuctionDto;
 import com.leehk.auction.domain.auction.infrastructure.AuctionEntity;
 
 public class AuctionConverter {
@@ -15,6 +16,32 @@ public class AuctionConverter {
                 .startTime(auctionEntity.getStartTime())
                 .endTime(auctionEntity.getEndTime())
                 .status(auctionEntity.getStatus())
+                .build();
+    }
+
+    public static Auction DtoToDomain(AuctionDto auctionDto) {
+        return Auction.builder()
+                .id(auctionDto.getId())
+                .title(auctionDto.getTitle())
+                .description(auctionDto.getDescription())
+                .startPrice(auctionDto.getStartPrice())
+                .currentPrice(auctionDto.getCurrentPrice())
+                .startTime(auctionDto.getStartTime())
+                .endTime(auctionDto.getEndTime())
+                .status(auctionDto.getStatus())
+                .build();
+    }
+    
+    public static AuctionDto DomainToDto(Auction auction) {
+        return AuctionDto.builder()
+                .id(auction.getId())
+                .title(auction.getTitle())
+                .description(auction.getDescription())
+                .startPrice(auction.getStartPrice())
+                .currentPrice(auction.getCurrentPrice())
+                .startTime(auction.getStartTime())
+                .endTime(auction.getEndTime())
+                .status(auction.getStatus())
                 .build();
     }
 }

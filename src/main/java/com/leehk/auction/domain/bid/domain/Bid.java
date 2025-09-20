7819ec674c3796a37c05a168b71d8db9f4a04c41
이학준 <dlgkrwns213@naver.com>
2025-09-21 00:55:07 +0900

    feat: Bid domain 선언

---
+++ src/main/java/com/leehk/auction/domain/bid/domain/Bid.java
@@ -0,0 +1,23 @@
+package com.leehk.auction.domain.bid.domain;
+
+import com.leehk.auction.domain.auction.domain.Auction;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class Bid {
+
+    private Long id;
+    private Long bidderId;
+    private long bidPrice;
+    private LocalDateTime bidTime;
+    private Auction auction;
+
+    public Bid(Long bidderId, long bidPrice, Auction auction) {
+        this.bidderId = bidderId;
+        this.bidPrice = bidPrice;
+        this.auction = auction;
+        this.bidTime = LocalDateTime.now();
+    }
+}

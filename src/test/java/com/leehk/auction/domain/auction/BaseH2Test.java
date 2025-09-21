528d33479f998f76f886bad61a6300571fc19552
이학준 <dlgkrwns213@naver.com>
2025-09-21 15:12:31 +0900

    test: SpringBootTest 와 application-test.yml 을 참조하는 h2Test class 선언

---
+++ src/test/java/com/leehk/auction/domain/auction/BaseH2Test.java
@@ -0,0 +1,9 @@
+package com.leehk.auction.domain.auction;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class BaseH2Test {
+}

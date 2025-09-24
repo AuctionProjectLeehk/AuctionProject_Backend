af01020c513e04c602d6c7a1ef3391e5a2fa7ee5
이학준 <dlgkrwns213@naver.com>
2025-09-24 17:46:20 +0900

    feat: Bid Entity에서 UUID 설정

--- src/main/java/com/leehk/auction/domain/bid/infrastructure/BidEntity.java
+++ src/main/java/com/leehk/auction/domain/bid/infrastructure/BidEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "bid")
@@ -16,8 +17,7 @@ import java.time.LocalDateTime;
 public class BidEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     private Long bidderId;
 

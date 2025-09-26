c405bbe832e3c6fc949a6fe6467f8720ed4327cd
이학준 <dlgkrwns213@naver.com>
2025-09-26 17:43:31 +0900

    feat: test 를 위해 @AllArgsConstructor 추가

--- src/main/java/com/leehk/auction/domain/user/dto/SignUpRequestDto.java
+++ src/main/java/com/leehk/auction/domain/user/dto/SignUpRequestDto.java
@@ -1,10 +1,12 @@
 package com.leehk.auction.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor  // test 용
 public class SignUpRequestDto {
 
     private String email;

b73a9ccfda77fba6f16c74846c255b4ebb78b294
이학준 <dlgkrwns213@naver.com>
2025-09-26 18:24:44 +0900

    test: H2 에서도 포트 충돌 막기

--- src/test/java/com/leehk/auction/domain/user/application/AuthServiceImplTest.java
+++ src/test/java/com/leehk/auction/domain/user/application/AuthServiceImplTest.java
@@ -7,9 +7,11 @@ import com.leehk.auction.domain.user.dto.SignUpRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AuthServiceImplTest extends BaseH2Test {
 
     @Autowired

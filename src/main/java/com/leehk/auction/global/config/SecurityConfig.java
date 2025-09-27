0b8a81ec45375861b7b1b6581189c819227c128c
이학준 <dlgkrwns213@naver.com>
2025-09-27 23:55:38 +0900

    feat: auctions 를 JWT로 접근하도록 설정

--- src/main/java/com/leehk/auction/global/config/SecurityConfig.java
+++ src/main/java/com/leehk/auction/global/config/SecurityConfig.java
@@ -1,33 +1,38 @@
 package com.leehk.auction.global.config;
 
+import com.leehk.auction.global.auth.JwtAuthenticationFilter;
+import com.leehk.auction.global.auth.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(
-//                                "/swagger-ui.html",
-//                                "/swagger-ui/**",
-//                                "/v3/api-docs.yaml",
-//                                "/v3/api-docs/**",
-//                                "/swagger-resources/**",
-//                                "/webjars/**"
-//                        ).permitAll()
-//                        .anyRequest().authenticated()
-                                .anyRequest().permitAll()
-                );
+                        .requestMatchers("/auctions/**").authenticated() // 인증 필요
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider);
     }
 }

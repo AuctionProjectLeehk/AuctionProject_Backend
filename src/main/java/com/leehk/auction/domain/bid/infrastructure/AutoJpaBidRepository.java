e7e711d40a46044c3983136aa7da705f9aa58e2f
이학준 <dlgkrwns213@naver.com>
2025-10-01 16:21:36 +0900

    fix: AutoBidRepository 선언

---
+++ src/main/java/com/leehk/auction/domain/bid/infrastructure/AutoJpaBidRepository.java
@@ -0,0 +1,11 @@
+package com.leehk.auction.domain.bid.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AutoJpaBidRepository extends AutoBidRepository, JpaRepository<AutoBidEntity, UUID> {
+}
+

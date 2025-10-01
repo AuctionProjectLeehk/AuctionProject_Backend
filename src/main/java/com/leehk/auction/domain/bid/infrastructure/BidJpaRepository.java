7bf5ace10e6a2d931e2dc357c93ad3b8c6e82d53
이학준 <dlgkrwns213@naver.com>
2025-10-01 16:21:17 +0900

    fix: id type 변경

--- src/main/java/com/leehk/auction/domain/bid/infrastructure/BidJpaRepository.java
+++ src/main/java/com/leehk/auction/domain/bid/infrastructure/BidJpaRepository.java
@@ -3,6 +3,8 @@ package com.leehk.auction.domain.bid.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface BidJpaRepository extends BidRepository, JpaRepository<BidEntity, Long> {
+public interface BidJpaRepository extends BidRepository, JpaRepository<BidEntity, UUID> {
 }

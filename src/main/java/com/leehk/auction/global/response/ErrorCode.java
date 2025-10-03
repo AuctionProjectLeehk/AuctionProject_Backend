1731a4ff898905e65084ecb9ab3ab2da86d39750
이학준 <dlgkrwns213@naver.com>
2025-10-04 01:23:04 +0900

    feat: 자동 입찰을 찾을 수 없는 에러 코드 추가

--- src/main/java/com/leehk/auction/global/response/ErrorCode.java
+++ src/main/java/com/leehk/auction/global/response/ErrorCode.java
@@ -32,6 +32,7 @@ public enum ErrorCode {
     USER_NOT_FOUND(40401, HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
     AUCTION_NOT_FOUND(40402, HttpStatus.NOT_FOUND, "경매를 찾을 수 없습니다."),
     BID_NOT_FOUND(40403, HttpStatus.NOT_FOUND, "입찰을 찾을 수 없습니다."),
+    AUTO_BID_NOT_FOUND(40404, HttpStatus.NOT_FOUND, "자동 입찰을 찾을 수 없습니다."),
     NOT_FOUND(40400, HttpStatus.NOT_FOUND, "찾을 수 없습니다."),
 
     // 409 Conflict

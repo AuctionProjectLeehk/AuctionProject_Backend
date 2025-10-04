4d30c0d3857de325fcce851aac7832130ae58f66
이학준 <dlgkrwns213@naver.com>
2025-10-04 23:15:44 +0900

    feat: AutoBidEntity에 AuctionEntity와의 관계를 위한 JsonBackReference 추가 및 메서드 오타 수정

--- src/main/java/com/leehk/auction/domain/bid/infrastructure/AutoBidEntity.java
+++ src/main/java/com/leehk/auction/domain/bid/infrastructure/AutoBidEntity.java
@@ -1,5 +1,6 @@
 package com.leehk.auction.domain.bid.infrastructure;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.leehk.auction.domain.auction.infrastructure.AuctionEntity;
 import com.leehk.auction.domain.bid.domain.AutoBid;
 import jakarta.persistence.*;
@@ -33,9 +34,10 @@ public class AutoBidEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "auction_id")
+    @JsonBackReference
     private AuctionEntity auctionEntity;
 
-    public void udpateAuctionEntity(AuctionEntity auctionEntity) {
+    public void updateAuctionEntity(AuctionEntity auctionEntity) {
         this.auctionEntity = auctionEntity;
     }
 

9297a1b9677050874f90036965a4606c93eb95c1
이학준 <dlgkrwns213@naver.com>
2025-10-07 16:02:01 +0900

    feat: TransactionType 선언

---
+++ src/main/java/com/leehk/auction/domain/wallet/enums/TransactionType.java
@@ -0,0 +1,8 @@
+package com.leehk.auction.domain.wallet.enums;
+
+public enum TransactionType {
+    DEPOSIT,        // 충전
+    WITHDRAW,       // 출금
+    TRANSFER,       // 송금
+    REFUND          // 환불
+}

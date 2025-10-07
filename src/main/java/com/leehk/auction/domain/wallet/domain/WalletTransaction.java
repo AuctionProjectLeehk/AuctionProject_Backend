cd24484b676a9d0e54549a07ef7d06d28eb4df57
이학준 <dlgkrwns213@naver.com>
2025-10-07 16:04:08 +0900

    feat: WalletTransaction VO 선언

---
+++ src/main/java/com/leehk/auction/domain/wallet/domain/WalletTransaction.java
@@ -0,0 +1,25 @@
+package com.leehk.auction.domain.wallet.domain;
+
+import com.leehk.auction.domain.money.domain.Money;
+import com.leehk.auction.domain.wallet.enums.TransactionType;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+public class WalletTransaction {
+    private final UUID walletId;
+    private final Money money;
+    private final TransactionType type;
+    private final LocalDateTime createdAt;
+
+    public static WalletTransaction create(UUID walletId, Money money, TransactionType type) {
+        return WalletTransaction.builder()
+                .walletId(walletId)
+                .money(money)
+                .type(type)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}

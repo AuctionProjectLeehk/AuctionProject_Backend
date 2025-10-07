66adcc64a038166a1771016f6964c9e118ce02cd
이학준 <dlgkrwns213@naver.com>
2025-10-07 17:53:17 +0900

    feat: WalletTransactionConverter 클래스 추가 및 변환 메서드 정의

---
+++ src/main/java/com/leehk/auction/domain/wallet/converter/WalletTransactionConverter.java
@@ -0,0 +1,40 @@
+package com.leehk.auction.domain.wallet.converter;
+
+import com.leehk.auction.domain.money.domain.Money;
+import com.leehk.auction.domain.wallet.domain.WalletTransaction;
+import com.leehk.auction.domain.wallet.infrastructure.WalletEntity;
+import com.leehk.auction.domain.wallet.infrastructure.WalletTransactionEntity;
+
+public class WalletTransactionConverter {
+
+    /**
+     * WalletTransactionEntity -> WalletTransaction 도메인 변환
+     *
+     * @param walletTransactionEntity WalletTransactionEntity
+     * @return WalletTransaction 도메인 객체
+     */
+    public static WalletTransaction entityToDomain(WalletTransactionEntity walletTransactionEntity) {
+        return WalletTransaction.builder()
+                .walletId(walletTransactionEntity.getWallet().getPublicId())
+                .money(new Money(walletTransactionEntity.getAmount()))
+                .type(walletTransactionEntity.getTransactionType())
+                .createdAt(walletTransactionEntity.getCreateAt())
+                .build();
+    }
+
+    /**
+     * WalletTransaction 도메인 -> WalletTransactionEntity 변환
+     *
+     * @param walletTransaction WalletTransaction 도메인 객체
+     * @param walletEntity      WalletEntity 연관관계
+     * @return WalletTransactionEntity
+     */
+    public static WalletTransactionEntity domainToEntity(WalletTransaction walletTransaction, WalletEntity walletEntity) {
+        return WalletTransactionEntity.builder()
+                .wallet(walletEntity)
+                .transactionType(walletTransaction.getType())
+                .amount(walletTransaction.getMoney().getAmount())
+                .createAt(walletTransaction.getCreatedAt())
+                .build();
+    }
+}

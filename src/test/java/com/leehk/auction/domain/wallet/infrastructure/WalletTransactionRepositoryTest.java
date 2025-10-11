458c2112c64d176e52a473d9e7b674100d6f9eb3
이학준 <dlgkrwns213@naver.com>
2025-10-12 01:15:29 +0900

    test: Wallet Transaction Repository test 구현

---
+++ src/test/java/com/leehk/auction/domain/wallet/infrastructure/WalletTransactionRepositoryTest.java
@@ -0,0 +1,137 @@
+package com.leehk.auction.domain.wallet.infrastructure;
+
+import com.leehk.auction.domain.money.domain.Money;
+import com.leehk.auction.domain.wallet.enums.TransactionType;
+import com.leehk.auction.domain.wallet.enums.WalletStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class WalletTransactionRepositoryTest {
+
+    @Autowired
+    private WalletTransactionRepository walletTransactionRepository;
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    private WalletEntity makeWalletEntity(long index) {
+        return WalletEntity.builder()
+                .publicId(UUID.randomUUID())
+                .walletName("testWallet" + index)
+                .userId(100L)
+                .money(new Money(10000L))
+                .walletStatus(WalletStatus.ACTIVE)
+                .build();
+    }
+
+    @Test
+    @DisplayName("거래 내역 저장 - 성공")
+    void saveWalletTransaction_Success() {
+        // given
+        WalletEntity walletEntity = walletRepository.save(makeWalletEntity(1L));
+
+        // when
+        WalletTransactionEntity transaction1 = WalletTransactionEntity.builder()
+                .wallet(walletEntity)
+                .transactionType(TransactionType.DEPOSIT)
+                .amount(1000L)
+                .build();
+
+        WalletTransactionEntity savedTransaction = walletTransactionRepository.save(transaction1);
+
+        // then
+        assertThat(savedTransaction).isNotNull();
+        assertThat(savedTransaction.getId()).isEqualTo(transaction1.getId());
+        assertThat(savedTransaction.getWallet()).isEqualTo(walletEntity);
+    }
+    
+    @Test
+    @DisplayName("지갑에 저장된 모든 거래 내역 찾기 - 성공")
+    void findByWallet_Success() {
+        // given
+        WalletEntity walletEntity = walletRepository.save(makeWalletEntity(1L));
+
+        WalletTransactionEntity transaction1 = WalletTransactionEntity.builder()
+                .wallet(walletEntity)
+                .transactionType(TransactionType.DEPOSIT)
+                .amount(1000L)
+                .build();
+        WalletTransactionEntity transaction2 = WalletTransactionEntity.builder()
+                .wallet(walletEntity)
+                .transactionType(TransactionType.TRANSFER)
+                .amount(100L)
+                .build();
+        WalletTransactionEntity transaction3 = WalletTransactionEntity.builder()
+                .wallet(walletEntity)
+                .transactionType(TransactionType.WITHDRAW)
+                .amount(200L)
+                .build();
+
+        walletRepository.save(walletEntity);
+        walletTransactionRepository.save(transaction1);
+        walletTransactionRepository.save(transaction2);
+        walletTransactionRepository.save(transaction3);
+
+        // when
+        List<WalletTransactionEntity> transactions = walletTransactionRepository.findByWallet(walletEntity);
+
+        // then
+        assertThat(transactions.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("지갑에 저장된 모든 거래 내역을 생성 날짜 순으로 찾기 - 성공")
+    void findByWalletOrderByCreatedAtDesc_Success() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+
+        WalletEntity walletEntity = walletRepository.save(makeWalletEntity(1L));
+
+        WalletTransactionEntity transaction1 = WalletTransactionEntity.builder()
+                .wallet(walletEntity)
+                .transactionType(TransactionType.DEPOSIT)
+                .amount(1000L)
+                .createAt(now.plusDays(20))
+                .build();
+        WalletTransactionEntity transaction2 = WalletTransactionEntity.builder()
+                .wallet(walletEntity)
+                .transactionType(TransactionType.TRANSFER)
+                .createAt(now.plusDays(10))
+                .amount(100L)
+                .build();
+        WalletTransactionEntity transaction3 = WalletTransactionEntity.builder()
+                .wallet(walletEntity)
+                .transactionType(TransactionType.WITHDRAW)
+                .amount(200L)
+                .createAt(now.plusDays(15))
+                .build();
+
+        walletRepository.save(walletEntity);
+        walletTransactionRepository.save(transaction1);
+        walletTransactionRepository.save(transaction2);
+        walletTransactionRepository.save(transaction3);
+
+        // when
+        List<WalletTransactionEntity> orderedTransactions = walletTransactionRepository.findByWalletOrderByCreateAtDesc(walletEntity);
+
+        // then: 1 3 2 순서 확인
+        assertThat(orderedTransactions.size()).isEqualTo(3);
+        assertThat(orderedTransactions.stream()
+                .map(WalletTransactionEntity::getCreateAt)
+                .toArray()).isEqualTo(new LocalDateTime[]{now.plusDays(20), now.plusDays(15), now.plusDays(10)});
+        assertThat(orderedTransactions.stream()
+                .map(WalletTransactionEntity::getId)
+                .toArray()).isEqualTo(new UUID[]{transaction1.getId(), transaction3.getId(), transaction2.getId()});
+    }
+
+}
